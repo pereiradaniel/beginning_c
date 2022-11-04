80275b78bf72e0d30fe63551761050905763d84c
Daniel Pereira <danielpaulpereira@protonmail.com>
2022-11-04 16:54:23 -0400

    Add limits.c

---
+++ ch2/limits.c
@@ -0,0 +1,12 @@
+// Limits
+// Daniel Pereira, 04 November 2022.
+
+#include <stdio.h>
+#include <limits.h> // limits on integer types
+#include <float.h>  // limits on floating point types
+
+int main(int argc, char* argv[])
+{
+
+    return 0;
+}
