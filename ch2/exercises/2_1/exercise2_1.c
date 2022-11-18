af3f34004e18e2206c27ba4e7c6dca4e0f4b571d
Daniel Pereira <danielpaulpereira@protonmail.com>
2022-11-18 12:48:50 -0500

    Works

--- ch2/exercises/2_1/exercise2_1.c
+++ ch2/exercises/2_1/exercise2_1.c
@@ -9,12 +9,27 @@
 
 int main(int argc, char* argv[])
 {
+    double input = 0, inches = 0, yards = 0, feet = 0, rem = 0;
 
     // Prompt user for input in inches:
+    printf("Enter number of inches for conversion to yards, feet, inches: ");
+    scanf(" %lf", &input);
+
+    inches = input;
 
     // Convert inches into yards, feet, inches:
 
+    yards = inches / 36;        // Get number of yards:
+    rem = (int)inches % 36;     // Store remainder of inches.
+    inches -= yards * 36 - rem; // Subtract yards from total inches.
+
+    feet = inches / 12;        // Get number of feet:
+    rem = (int)inches % 12;    // Store remainder of inches.
+    inches -= feet * 12 - rem; // Subtract feet from total inches.
+
     // Display result:
+    printf("User's input: %f inches.\n", input);
+    printf("%f yards, %f feet, %f inches.\n", yards, feet, inches);
 
     return 0;
 }
