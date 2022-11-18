544ab2ae6a592c5704b4e871a0837e47c92a9187
Daniel Pereira <danielpaulpereira@protonmail.com>
2022-11-18 16:49:02 -0500

    Fix ch2 exercise2_1

--- ch2/exercises/2_1/exercise2_1.c
+++ ch2/exercises/2_1/exercise2_1.c
@@ -9,27 +9,29 @@
 
 int main(int argc, char* argv[])
 {
-    double input = 0, inches = 0, yards = 0, feet = 0, rem = 0;
+    // Constants for conversion process:
+    const int inches_per_foot = 12;
+    const int feet_per_yard   = 3;
+
+    // Variables for user input and calculations:
+    int input = 0, inches = 0, yards = 0, feet = 0;
 
     // Prompt user for input in inches:
     printf("Enter number of inches for conversion to yards, feet, inches: ");
-    scanf(" %lf", &input);
+    scanf(" %d", &input);
 
     inches = input;
 
     // Convert inches into yards, feet, inches:
+    feet   =  inches / inches_per_foot;
+    yards  =  feet   / feet_per_yard;
 
-    yards = inches / 36;        // Get number of yards:
-    rem = (int)inches % 36;     // Store remainder of inches.
-    inches -= yards * 36 - rem; // Subtract yards from total inches.
-
-    feet = inches / 12;        // Get number of feet:
-    rem = (int)inches % 12;    // Store remainder of inches.
-    inches -= feet * 12 - rem; // Subtract feet from total inches.
+    feet   %= feet_per_yard;    // Remainder of feet!
+    inches %= inches_per_foot;  // Remainder of inches!
 
     // Display result:
-    printf("User's input: %f inches.\n", input);
-    printf("%f yards, %f feet, %f inches.\n", yards, feet, inches);
+    printf("User's input: %d inches.\n", input);
+    printf("%d yards, %d feet, %d inches.\n", yards, feet, inches);
 
     return 0;
 }
