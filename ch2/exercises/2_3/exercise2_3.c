cfb003c539370617f85d29a8a1467abc8f13c5cb
Daniel Pereira <danielpaulpereira@protonmail.com>
2022-11-22 09:37:14 -0500

    Improve

--- ch2/exercises/2_3/exercise2_3.c
+++ ch2/exercises/2_3/exercise2_3.c
@@ -8,6 +8,8 @@
 
 #include <stdio.h>
 #include <limits.h> // Makes use of INT_MAX
+
+// FIXED PRODUCT PRICES:
 #define TYPE1 3.5f  // Type1 price
 #define TYPE2 5.5f  // Type2 price
 
@@ -22,13 +24,12 @@ int checkInput(int input, int min, int max);
 
 int main(int argc, char* argv[])
 {
-    int type = 0, quantity = 0;
-    double price = 0;
+    int type = 0, quantity = 0; // Vars for storing user input.
+    double price = 0;           // Stores calculated results.
 
     // USER SELECTS TYPE:
         // Can be either 1 or 2
         // Loops until input is valid and not == 0
-
     printf("Select product type (1 or 2): ");
     while(type == 0) {
         scanf(" %d", &type);
@@ -39,20 +40,14 @@ int main(int argc, char* argv[])
         // Min value is 1
         // Max value for quantity is largest possible INT value!
         // Loops until input is valid and not == 0
-
     printf("\nSelect quantity: ");
-    while(quantity == 0)
-    {
+    while(quantity == 0) {
         scanf(" %d", &quantity);
         quantity = checkInput(quantity, 1, INT_MAX);
     };
 
     // CALCULATE AND DISPLAY RESULTS:
-    
-    // Display user's selection:
-    calcStats(type, quantity);
-    
-    // Calculate and display results:
+    calcStats(type, quantity);    
     price = calculatePrice(type, quantity);
     display(type, quantity, price);
 
@@ -67,15 +62,12 @@ void calcStats(int type, int quantity)
 double calculatePrice(int input, int quantity)
 {
     double mult = 0;
-    switch(input)
-    {
-        case 1:
-            mult = TYPE1;
-            break;
-        case 2:
-            mult = TYPE2;
-            break;
-    }
+
+    if (input == 1)
+        mult = TYPE1;
+    if (input == 2)
+        mult = TYPE2;
+
     return mult * (double)quantity;
 }
 
@@ -88,9 +80,9 @@ int checkInput(int input, int min, int max)
 {
     int result = 0;
 
-    if (input < min || input > max) // out of bounds, invalid!
+    if (input < min || input > max)     // out of bounds, invalid!
         printf("\nEnter a number between %d or %d!\n", min, max);
-    else // inbounds, validate!
+    else                                // inbounds, validate!
         result = input;
 
     return result;
