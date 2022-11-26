bba8d7ff1a6cea682db6c82236df0265ade61568
Daniel Pereira <danielpaulpereira@protonmail.com>
2022-11-26 14:10:14 -0500

    Update exercise3_1

--- ch3/exercises/3_1/exercise3_1.c
+++ ch3/exercises/3_1/exercise3_1.c
@@ -9,23 +9,45 @@
 // from the conversion. To convert from Celsius to Fahrenheit you can multiply the value by 1.8 and then add 32.
 
 #include <stdio.h>
+#include <ctype.h>
+#define TOFARENHEIT(a) a*1.8+32.0
+#define TOCELSIUS(a) (a-32.0)/1.8
+
 int main(int argc, char* argv[])
 {
-    int choice = 0;
-    double temp = 0.0;
+    int choice = 0;     // Stores user's menu choice.
+    double temp = 0.0;  // Stores user's temperature input.
 
-    printf("Choose 1 or 2 from the following options:\n");
-    printf("\t1. Convert a temperature from degrees Celsius to degrees Fahrenheit.\n");
-    printf("\t2. Convert a temperature from degrees Fahrenheit to degrees Celsius.\n");
-    scanf(" %d", &choice);
+    char repeat = 'Y';
 
-    printf("Enter temperature to convert:\n");
-    scanf(" %lf", &temp);
+    while (toupper(repeat)=='Y')
+    {
+        choice = 0;
+        while (choice == 0)
+        {
+            printf("Choose 1 or 2 from the following options:\n");
+            printf("\t1. Convert a temperature from degrees Celsius to degrees Fahrenheit.\n");
+            printf("\t2. Convert a temperature from degrees Fahrenheit to degrees Celsius.\n");
+            scanf(" %d", &choice);
+            
+            if (choice != 1 && choice != 2)
+            {
+                printf("\nInvalid selection! Choose 1 or 2. Try again.\n");
+                choice = 0;
+            }
+        }
 
-    if (choice == 1)
-        printf("\nConversion: %.2lf fahreneit.\n", temp*1.8+32.0);
-    if (choice == 2)
-        printf("\nConversion: %.2lf celsius.\n", (temp-32.0)/1.8);
+        printf("Enter temperature to convert:\n");
+        scanf(" %lf", &temp);
+
+        if (choice == 1)
+            printf("\nConversion:\n%.2f celsius is %.2lf fahreneit.\n", temp, TOFARENHEIT(temp));
+        if (choice == 2)
+            printf("\nConversion:\n%.2f farenheit is %.2lf celsius.\n", temp, TOCELSIUS(temp));
+
+        printf("\nAgain? (Y to repeat, any other key to quit.)\n");
+        scanf(" %c", &repeat);
+    }
 
     return 0;
 }
