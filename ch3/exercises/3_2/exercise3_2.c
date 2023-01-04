cfec1045e0de8b42bf80549cbc783ff1a6c2bb9c
Daniel Pereira <danielpaulpereira@protonmail.com>
2023-01-04 14:11:02 -0500

    Add exercise3_2 and pseudocode.

---
+++ ch3/exercises/3_2/exercise3_2.c
@@ -0,0 +1,38 @@
+// Exercise 3-2
+
+// Write a program that prompts the user to enter the date as three integer
+// values for the month, the day in the month, and the year. The program should then output
+// the date in the form 31st December 2003 when the user enters 12 31 2003, for example.
+
+// You will need to work out when th, nd, st, and rd need to be appended to the day value. Don’t forget 1st, 2nd, 3rd,
+// 4th; but 11th, 12th, 13th, 14th; and 21st, 22nd, 23rd, and 24th.
+
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+    // INPUT
+    ////////
+
+    // Declare variables:
+        // month
+        // day
+        // year
+    
+    // Prompt user for input:
+        // month
+        // day
+        // year
+    
+
+    // OUTPUT
+    /////////
+
+    // Detect day and and append suffix, print:
+
+    // Detect month and print:
+
+    // Print year:
+
+    return 0;
+}
