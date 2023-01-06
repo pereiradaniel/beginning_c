45b25edfc73d0da2ca478dda765d28dae688d63c
Daniel Pereira <danielpaulpereira@protonmail.com>
2023-01-06 11:23:22 -0500

    Begin exercise3_4.c

---
+++ ch3/exercises/3_4/exercise3_4.c
@@ -0,0 +1,14 @@
+// Exercise 3-4
+
+// Modify the last example in the chapter that implemented a calculator so
+// that the user is given the option to enter y or Y to carry out another calculation, and n or
+// N to end the program. (Note: You’ll have to use a goto statement for this here, but you’ll
+// learn a better way of doing this in the next chapter.)
+
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+
+    return 0;
+}
