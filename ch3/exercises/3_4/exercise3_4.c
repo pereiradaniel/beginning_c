4c38482997b12a1d1f1da0eb833992d0f3cc47c7
Daniel Pereira <danielpaulpereira@protonmail.com>
2023-01-06 12:54:16 -0500

    Completed and add valgrind

--- ch3/exercises/3_4/exercise3_4.c
+++ ch3/exercises/3_4/exercise3_4.c
@@ -6,9 +6,62 @@
 // learn a better way of doing this in the next chapter.)
 
 #include <stdio.h>
+#include <ctype.h> // Modification: toupper(choice)
 
-int main(int argc, char* argv[])
+int main(void)
 {
+  double number1 = 0.0;  // First operand
+  double number2 = 0.0;  // Second operand
+  char operation = 0;    // Operation
 
-    return 0;
+start:
+    printf("\nEnter the calculation\n");
+    scanf("%lf %c %lf", &number1, &operation, &number2);
+
+  switch(operation)
+  {
+    case '+':
+      printf("= %lf\n", number1 + number2);
+      break;
+
+    case '-':
+      printf("= %lf\n", number1 - number2);
+      break;
+
+    case '*':
+      printf("= %lf\n", number1 * number2);
+      break;                              
+
+    case '/':
+    // Check for division by zero!!!
+      if(number2 == 0)
+        printf("\n\n\aDivision by zero error!\n");
+      else
+        printf("= %lf\n", number1 / number2);
+      break;
+
+    case '%':
+    // Check for division by zero!!!
+      if((long)number2 == 0) 
+         printf("\n\n\aDivision by zero error!\n");
+      else
+        printf("= %ld\n", (long)number1 % (long)number2);
+      break;
+
+    // Catch invalid operations
+    default:
+      printf("\n\n\aIllegal operation!\n");
+      break;
+  }
+
+  // MODIFICATION!!!
+  char choice = 'n';
+
+  printf("\nEnter y to repeat or any key to quit: ");
+  scanf(" %c", &choice);
+
+  if(toupper(choice) == 'Y')
+    goto start;
+
+  return 0;
 }
