55085a2e1e6e4e77e699031df31394280cb1e8cf
Daniel Pereira <danielpaulpereira@protonmail.com>
2023-01-10 15:41:35 -0500

    first draft

--- ch4/compact_for/compact.c
+++ ch4/compact_for/compact.c
@@ -6,6 +6,25 @@
 
 int main(int argc, char* argv[])
 {
+    unsigned int sum = 0, count = 0;
+    char choice = 'y';
+
+    do {
+        do {
+            printf("\nEnter a number from 1-100: ");
+            scanf(" %d", &sum);
+
+            if (sum == 0 || sum > 100)
+                printf("\nOut of bounds! Try again.\n");
+        } while(sum == 0 || sum > 100);
+
+        for (unsigned int i = 1; i <= count; sum += i++);
+
+        printf("\nTotal of the first %u numbers is %u\n", count, sum);
+
+        scanf(" %c", &choice);
+    } while(choice == 'y' || choice == 'Y');
+    
 
 
     return 0;
