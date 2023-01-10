2b4fbe6f06ef486ff40ecae1d1f1eaf37e2a8564
Daniel Pereira <danielpaulpereira@protonmail.com>
2023-01-10 16:10:49 -0500

    Final test draft

--- ch4/compact_for/compact.c
+++ ch4/compact_for/compact.c
@@ -7,32 +7,36 @@
 
 #include <stdio.h>
 #include <limits.h> // INT_MAX
+
 int main(int argc, char* argv[])
 {
     unsigned long long sum = 0;
     unsigned int count = 0;
     char choice = 'y';
 
-    do {
-        if (sum != 0)
+    do
+    {
+        if (sum != 0) // Reset sum if looping
             sum = 0;
 
-        do {
+        do
+        {
             printf("\nEnter a number from 1-%u: ", INT_MAX);
             scanf(" %u", &count);
 
             if (count == 0 || count > INT_MAX)
                 printf("\nOut of bounds! Try again.");
-        } while(count == 0 || count > INT_MAX);
+        }
+        while(count == 0 || count > INT_MAX);
 
-
-        for (unsigned int i = 1; i <= count; sum += i++);
+        for (unsigned int i = 1; i <= count; sum += i++); // compact for loop
 
         printf("\nTotal of the first %u numbers is %llu", count, sum);
 
         printf("\nAgain? (y\\n)\n");
         scanf(" %c", &choice);
-    } while(choice == 'y' || choice == 'Y');
+    }
+    while(choice == 'y' || choice == 'Y');
 
     return 0;
 }
