d0a2fda569a2b86ddbaf0b80049d4ae512f3d46e
pereiradaniel <danielpaulpereira@protonmail.com>
2023-01-11 15:42:59 -0500

    Begin exercise 4-1

---
+++ ch4/exercises/4_1/exercise4_1.c
@@ -0,0 +1,10 @@
+// Exercise 4-1
+// Write a program that will generate a multiplication table of a size entered
+// by the user.
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+
+    return 0;
+}
