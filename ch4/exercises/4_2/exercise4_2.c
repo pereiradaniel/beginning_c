a617ff4c57c33ef2ce7ca1a9b7bedb3629fd9a2c
Daniel Pereira <danielpaulpereira@protonmail.com>
2023-01-12 18:08:49 -0500

    Begin exercise4_2

---
+++ ch4/exercises/4_2/exercise4_2.c
@@ -0,0 +1,12 @@
+// Exercise 4-2
+// Write a program that will output the printable characters for character
+// code values from 0 to 127. Output each character code along with its symbol with two
+// characters to a line. Make sure the columns are aligned. (Hint: You can use the isgraph()
+// function that’s declared in ctype.h to determine when a character is printable.)
+
+#include <stdio.h>
+int main(int argc, char* argv[])
+{
+    
+    return 0;
+}
