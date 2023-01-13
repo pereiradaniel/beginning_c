17195a452888f73fde4bf75ac0a9809dc20f3b14
Daniel Pereira <danielpaulpereira@protonmail.com>
2023-01-12 20:00:49 -0500

    Exercise 4-2 complete

--- ch4/exercises/4_2/exercise4_2.c
+++ ch4/exercises/4_2/exercise4_2.c
@@ -6,41 +6,30 @@
 
 #include <stdio.h>
 #include <ctype.h>
-#define MAX_COLS 8
-#define MAX_CHAR_CODE 128
-char isPrintable(char c);
+
+#define MAX_COLS 8   // Maximum columns to allow
+#define CHAR_MAX 127 // Max char to print
+
 int main(int argc, char* argv[])
 {
-    int columns = 0;
+    int cols = 0; // columns for user input
     
-    while (columns == 0)
+    while (cols <= 0 || cols > MAX_COLS) // Loop until user input is within bounds
     {
         printf("\nThis program will output printable characters for code values from 0-127.");
         printf("\nHow many character/code pairs per row would you like to see? (1-%d): ", MAX_COLS);
-        scanf(" %d", &columns);
+        scanf(" %d", &cols);
 
-        if (columns <= 0 || columns > MAX_COLS)
-        {
-            printf("\n%d is out of bounds! Enter a number from 1-%d.", columns, MAX_COLS);
-            columns = 0;
-        }
+        if (cols <= 0 || cols > MAX_COLS) // Warn user if input is out of bounds
+            printf("\n%d is out of bounds! Enter a number from 1-%d.", cols, MAX_COLS);
     }
 
-    for(int i = 0; i < MAX_CHAR_CODE; i+=columns) // Loop through numbers 0-127
+    for(int i = 0; i <= CHAR_MAX; i+=cols) // Print row
     {
-        for (int j = 0; j<columns && (j+i) < MAX_CHAR_CODE; ++j) // Loop through columns
-        {
-            //if (i+j < MAX_CHAR_CODE)    
-            printf("%d\t%c\t", i+j, isPrintable(i+j));
-        }
-        
-        printf("\n"); // End of row
+        for (int j = 0; j<cols && (j+i) <= CHAR_MAX; ++j) // Print col
+            printf("%4d\t%4c\t", i+j, isgraph(i+j) ? i+j : ' ');
+        printf("\n");
     }
 
     return 0;
-}
-
-char isPrintable(char c)
-{
-    return isgraph(c) ? c : ' ';
 }
