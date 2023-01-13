334cda18e7d416c07b3e5687d7fd4fec096a0a89
Daniel Pereira <danielpaulpereira@protonmail.com>
2023-01-13 14:37:44 -0500

    Add 4_3

---
+++ ch4/exercises/4_3/exercise4_3.c
@@ -0,0 +1,11 @@
+// Exercise 4-3
+// Extend the previous program to output the appropriate name, such as
+// “newline”, “space”, “tab”, and so on, for each whitespace character.
+
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+
+    return 0;
+}
