b76b4ec3ec652ad834da4f40dbeaf16fa2589ce2
Daniel Pereira <danielpaulpereira@protonmail.com>
2023-01-15 14:49:14 -0500

    Begin refactor3

---
+++ ch4/exercises/4_3/refactor3.c
@@ -0,0 +1,8 @@
+// Exercise 4-3
+// Extend the previous program to output the appropriate name, such as
+// “newline”, “space”, “tab”, and so on, for each whitespace character.
+
+// REFACTOR: User selects range of char codes
+// REFACTOR2: Main program loops until user quits
+// REFACTOR3: Count and report how many non-printable chars.
+
