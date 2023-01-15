c17bc35d485d558497332ba0621be40e520481ae
Daniel Pereira <danielpaulpereira@protonmail.com>
2023-01-15 15:22:22 -0500

    Added refactor3

--- ch4/exercises/4_3/refactor3.c
+++ ch4/exercises/4_3/refactor3.c
@@ -6,3 +6,117 @@
 // REFACTOR2: Main program loops until user quits
 // REFACTOR3: Count and report how many non-printable chars.
 
+#include <stdio.h>
+#include <ctype.h>
+#include <stdbool.h>
+
+#define MAX_COLS 8   // Maximum columns to allow
+#define CHAR_MAX 127 // Max char to print
+
+void decodeChar(char c); // Prints a label for non printable characters.
+void printTable(int min_char, int max_char, int cols, int np); // Prints character code table.
+void warnUser(int min_char, int max_char); // Warns user if input for table is invalid.
+
+int main(int argc, char* argv[])
+{
+    int cols = 0;       // columns for user input
+    int min_char = 0;   // min range for char codes
+    int max_char = 0;   // max range for char codes
+
+    int np = 0;
+
+    bool repeat = true; // main repeat
+    char again = 'n';   // user choice
+
+    while (repeat == true)
+    {
+        while (cols <= 0 || cols > MAX_COLS)    // Loop until user input is within bounds
+        {
+            printf("\nThis program will output printable characters for code values from 0-%d.", CHAR_MAX);
+            printf("\nHow many columns of character/code pairs per row would you like to see? (1-%d): ", MAX_COLS);
+            scanf(" %d", &cols);
+
+            if (cols <= 0 || cols > MAX_COLS)   // Warn user if input is out of bounds
+                printf("\n%d is out of bounds! Enter a number from 1-%d.", cols, MAX_COLS);
+        }
+        
+        while ((max_char <= 0 && min_char <= 0) || min_char >= max_char || max_char > CHAR_MAX || min_char == max_char)    // Loop until min and max char are valid
+        {
+            printf("\nWhat range of char codes do you want? (Range 0-%d, example input \"0 %d\"): ", CHAR_MAX, CHAR_MAX);
+            scanf(" %d%d", &min_char, &max_char);
+
+            warnUser(min_char, max_char);
+        }
+
+        printTable(min_char, max_char, cols, np);   // Print character code table
+        
+        printf("\n");
+
+        printf("\nAgain ? (y/n): ");
+        scanf(" %c", &again);
+
+        if (again == 'n' || again == 'N')
+            repeat = false;
+        else if (again == 'y' || 'Y')
+            max_char = 0, min_char = 0, cols = 0, np = 0;  // Reset values for another run
+    };
+    
+    return 0;
+}
+
+void warnUser(int min_char, int max_char)
+{
+    if (min_char < 0)
+        printf("\n%d is out of bounds! Minimum number must be from  0-%d.", min_char, CHAR_MAX-1);
+    if (max_char > CHAR_MAX)
+        printf("\n%d is out of bounds! Maximum number must be less than 0-%d.", max_char, CHAR_MAX+1);
+    if (min_char > max_char)
+        printf("\n%d greater than %d! Minimum number must be less than maximum.", min_char, max_char);
+    if (min_char == max_char)
+        printf("\n%d is equal to %d! Minimum number must be less than maximum.", min_char, max_char);
+}
+
+void printTable(int min_char, int max_char, int cols, int np)
+{
+    for(int i = min_char, j = 0; i <= max_char; ++i, ++j)      // Decode and print a character.
+    {
+        if (j%cols==0)
+            printf("\n");                   // Print new line if num of cols reached.
+        
+        printf("  %4d",i);                  // Print code number
+        
+        if (isgraph(i))
+            printf("               %c",i);  // Prints printable character
+        else
+        {
+            decodeChar(i);                  // Returns label for non-printable character
+            ++np;                           // count a non-printable char
+        }
+    }
+    printf("\n%d non printable chars were in the output.", np);
+}
+
+void decodeChar(char c) // Prints a label for non printable characters.
+{
+    switch(c)
+    {
+        case '\n':
+            printf("         newline");
+            break;
+        case ' ':
+            printf("           space");
+            break;
+        case '\t':
+            printf("  horizontal tab");
+            break;
+        case '\v':
+            printf("    vertical tab");
+            break;
+            case '\f':
+            printf("       form feed");
+            break;
+        default:
+            printf("                ");
+            break;
+    }
+}
